737881464faaded33903925b73cef77b0c8d1940
hookjabber <pat2709783@gmail.com>
2025-09-18 15:42:46 +0300

    correct train mclsr logic handled

--- src/train_mclsr.py
+++ src/train_mclsr.py
@@ -8,10 +8,10 @@ from utils import (
 
 from callbacks import CompositeCallback, InferenceCallback, MetricCallback
 from dataset import MCLSRDataset, GraphDataset
-from loss import SASRecLoss
 from metric import HitRateMetric, NDCGMetric, RecallMetric
 from models import MCLSR
 from optimizer import Optimizer
+from loss import CompositeLoss, SamplesSoftmaxLoss, FpsLoss
 
 import copy
 import json
@@ -24,10 +24,9 @@ from torch.utils.data import DataLoader
 from dataloader import batch_processor
 from train_base import train
 
-
 LOGGER = create_logger(name=__name__)
 SEED_VAL = 42
-EXPERIMENT_NAME = 'sasrec_clothing_all_data_their'
+EXPERIMENT_NAME = 'mclsr_Clothing'
 
 TRAIN_BATCH_SIZE = 128
 VALID_BATCH_SIZE = 128
@@ -47,10 +46,10 @@ def main():
     tensorboard_writer = utils.tensorboards.TensorboardWriter(EXPERIMENT_NAME)
     utils.tensorboards.GLOBAL_TENSORBOARD_WRITER = tensorboard_writer
     
-    dataset = MCLSRDataset(data_dir_path='../data/Clothing', num_negatives=1280)
+    dataset = MCLSRDataset(data_dir_path='./data/Clothing', num_negatives=1280)
     graph_dataset = GraphDataset(
         dataset=dataset,
-        graph_dir_path='../data/Clothing',
+        graph_dir_path='./data/Clothing',
         use_user_graph=True,
         use_item_graph=True,
         neighborhood_size=50
@@ -90,9 +89,48 @@ def main():
         dropout=DROPOUT,
         layer_norm_eps=1e-9,
         graph_dropout=DROPOUT,
-    )
+    ).to(DEVICE)
 
-    # loss = SASRecLoss(output_prefix='loss')
+    loss = CompositeLoss(
+    losses=[
+        SamplesSoftmaxLoss(
+            queries_prefix="combined_representation",
+            positive_prefix="label_representation",
+            negative_prefix="negative_representation",
+            output_prefix="downstream_loss",
+        ),
+
+        FpsLoss(
+            fst_embeddings_prefix="sequential_representation",
+            snd_embeddings_prefix="graph_representation",
+            tau=0.5,
+            normalize_embeddings=True,
+            use_mean=True,
+            output_prefix="contrastive_interest_loss",
+        ),
+
+        FpsLoss(
+            fst_embeddings_prefix="user_graph_user_embeddings",
+            snd_embeddings_prefix="common_graph_user_embeddings",
+            tau=0.5,
+            normalize_embeddings=True,
+            use_mean=True,
+            output_prefix="contrastive_user_feature_loss",
+        ),
+
+        FpsLoss(
+            fst_embeddings_prefix="item_graph_item_embeddings",
+            snd_embeddings_prefix="common_graph_item_embeddings",
+            tau=0.5,
+            normalize_embeddings=True,
+            use_mean=True,
+            output_prefix="contrastive_item_feature_loss",
+        ),
+    ],
+    weights=[1.0, 1.0, 0.05, 0.05],
+    output_prefix="loss",
+)
+
 
     optimizer = Optimizer(
         model=model,
@@ -101,53 +139,53 @@ def main():
         lr=0.001
     )
     
-    # callback = CompositeCallback(
-    #     callbacks=[
-    #         MetricCallback(
-    #             on_step=1,
-    #             value_prefix='loss'
-    #         ),
-    #         InferenceCallback(
-    #             model=model,
-    #             dataloader=valid_dataloader,
-    #             on_step=64,
-    #             metrics={
-    #                 f'{metric_name}@{k}': metric_cls(k=k)
-    #                 for metric_cls, metric_name in [(HitRateMetric, 'hit'), (NDCGMetric, 'ndcg'), (RecallMetric, 'recall')] 
-    #                 for k in [5, 10, 20, 50]
-    #             },
-    #             metric_prefix='validation'
-    #         ),
-    #         InferenceCallback(
-    #             model=model,
-    #             dataloader=test_dataloader,
-    #             on_step=256,
-    #             metrics={
-    #                 f'{metric_name}@{k}': metric_cls(k=k)
-    #                 for metric_cls, metric_name in [(HitRateMetric, 'hit'), (NDCGMetric, 'ndcg'), (RecallMetric, 'recall')] 
-    #                 for k in [5, 10, 20, 50]
-    #             },
-    #             metric_prefix='eval'
-    #         )
-    #     ]
-    # )
+    callback = CompositeCallback(
+        callbacks=[
+            MetricCallback(
+                on_step=1,
+                value_prefix='loss'
+            ),
+            InferenceCallback(
+                model=model,
+                dataloader=valid_dataloader,
+                on_step=64,
+                metrics={
+                    f'{metric_name}@{k}': metric_cls(k=k)
+                    for metric_cls, metric_name in [(HitRateMetric, 'hit'), (NDCGMetric, 'ndcg'), (RecallMetric, 'recall')] 
+                    for k in [5, 10, 20, 50]
+                },
+                metric_prefix='validation'
+            ),
+            InferenceCallback(
+                model=model,
+                dataloader=test_dataloader,
+                on_step=256,
+                metrics={
+                    f'{metric_name}@{k}': metric_cls(k=k)
+                    for metric_cls, metric_name in [(HitRateMetric, 'hit'), (NDCGMetric, 'ndcg'), (RecallMetric, 'recall')] 
+                    for k in [5, 10, 20, 50]
+                },
+                metric_prefix='eval'
+            )
+        ]
+    )
 
-    # LOGGER.debug('Everything is ready for training process!')
+    LOGGER.debug('Everything is ready for training process!')
 
     # Train process
-    # best_model = train(
-    #     dataloader=train_dataloader,
-    #     model=model,
-    #     optimizer=optimizer,
-    #     loss_function=loss,
-    #     callback=callback,
-    # )
+    best_model = train(
+        dataloader=train_dataloader,
+        model=model,
+        optimizer=optimizer,
+        loss_function=loss,
+        callback=callback,
+    )
 
-    # LOGGER.debug('Saving model...')
-    # ensure_checkpoints_dir()
-    # checkpoint_path = f'./checkpoints/{EXPERIMENT_NAME}_best_state.pth'
-    # torch.save(best_model.state_dict(), checkpoint_path)
-    # LOGGER.debug(f'Model saved as {checkpoint_path}')
+    LOGGER.debug('Saving model...')
+    ensure_checkpoints_dir()
+    checkpoint_path = f'./checkpoints/{EXPERIMENT_NAME}_best_state.pth'
+    torch.save(best_model.state_dict(), checkpoint_path)
+    LOGGER.debug(f'Model saved as {checkpoint_path}')
 
 
 if __name__ == '__main__':
