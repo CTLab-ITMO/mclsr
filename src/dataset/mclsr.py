fix negative sampling, extended subsequences range, eval score masking

src/dataset/mclsr.py
@@ -1,15 +1,17 @@
 import copy
-import numpy as np
+import random
 
 from dataset.base import TrainSampler, EvalSampler, BaseSequenceDataset
 
 
 class MCLSRTrainSampler(TrainSampler):
-    def __init__(self, dataset, num_users, num_items, num_negatives):
+    def __init__(self, dataset, num_users, num_items, num_negatives, user_to_all_seen_items):
         super().__init__(dataset)
         self._num_users = num_users
         self._num_items = num_items
         self._num_negatives = num_negatives
+        self._all_items_set = set(range(1, num_items + 1))
+        self._user_to_all_seen_items = user_to_all_seen_items
 
     def __getitem__(self, index):
         sample = copy.deepcopy(self._dataset[index])
@@ -17,7 +19,9 @@ class MCLSRTrainSampler(TrainSampler):
         item_sequence = sample['item.ids'][:-1]
         positive_item = sample['item.ids'][-1]
 
-        negatives = np.random.randint(0, self._num_items + 1, (self._num_negatives,)).tolist()
+        user_id = sample['user.ids'][0]
+        unseen_items = list(self._all_items_set - self._user_to_all_seen_items[user_id])
+        negatives = random.sample(unseen_items, self._num_negatives)
 
         return {
             'user.ids': sample['user.ids'],
@@ -42,13 +46,21 @@ class MCLSRDataset(BaseSequenceDataset):
     def __init__(self, data_dir_path, num_negatives):
         super().__init__(data_dir_path=data_dir_path, train_extended=True)
         self._num_negatives = num_negatives
-    
+
+        # Build user -> all seen items mapping for negative sampling
+        from collections import defaultdict
+        self._user_to_all_seen_items = defaultdict(set)
+        for sample in self._train_dataset:
+            user_id = sample['user.ids'][0]
+            self._user_to_all_seen_items[user_id].update(sample['item.ids'])
+
     def get_samplers(self):
         train_sampler = MCLSRTrainSampler(
-            dataset=self._train_dataset, 
-            num_users=self._num_users, 
+            dataset=self._train_dataset,
+            num_users=self._num_users,
             num_items=self._num_items,
-            num_negatives=self._num_negatives
+            num_negatives=self._num_negatives,
+            user_to_all_seen_items=self._user_to_all_seen_items
         )
         valid_sampler = MCLSREvalSampler(
             dataset=self._valid_dataset, 

src/models/mclsr.py
@@ -354,12 +354,16 @@ class MCLSR(BaseModel):
             # formula 16: R(u,N) = Top-N((I_s)^T * h_o)
             candidate_embeddings = (
                 self._item_embeddings.weight
-            )  # (num_items, embedding_dim)
+            )  # (num_items + 2, embedding_dim)
             candidate_scores = torch.einsum(
                 'bd,nd->bn',
                 sequential_representation,  # I_s
                 candidate_embeddings,  # all h_v
-            )  # (batch_size, num_items)
+            )  # (batch_size, num_items + 2)
+
+            # Mask padding (index 0) and special tokens (index num_items+1 onwards)
+            candidate_scores[:, 0] = -torch.inf
+            candidate_scores[:, self._num_items + 1:] = -torch.inf
 
             _, indices = torch.topk(
                 candidate_scores,